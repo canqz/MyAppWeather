bf52ee42775cb88b1cb7fd79d3fce96728d74e86
canqz <990187556@qq.com>
2020-11-30 22:50:22 +0800

    现在好像api不能用了

--- app/src/main/java/com/example/myweather/fragment/MainViewPagerFragment.java
+++ app/src/main/java/com/example/myweather/fragment/MainViewPagerFragment.java
@@ -16,17 +16,23 @@ import androidx.fragment.app.Fragment;
 
 import com.example.myweather.R;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.io.IOException;
 
+import okhttp3.Call;
+import okhttp3.Callback;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
 
+
 public class MainViewPagerFragment extends Fragment{
     Context context;
     String city;
     String url;
+    String TAG = "MainViewPagerFragment";
 
     String url1 = "http://api.map.baidu.com/telematics/v3/weather?location=";
     String url2 = "&output=json&ak=FkPhtMBK0HTIQNh7gG4cNUttSTyr0nzo";
@@ -47,41 +53,32 @@ public class MainViewPagerFragment extends Fragment{
     }
 
     private void loadData() {
-        new MyNetJob().execute();
-    }
+        //先有一个客户端
+        OkHttpClient client = new OkHttpClient();
 
+        //再有一个请求
+        Request request = new Request.Builder()
+                .get()
+                .url(url)
+                .build();
 
-    class MyNetJob extends AsyncTask<String,Void,String>{
+        //Call将客户端和请求这两者连到一起
+        Call call = client.newCall(request);
 
-        //这里做网络耗时的操作
-        @Override
-        protected String doInBackground(String... strings) {
-            String result = null;
-            if (url == null) {
-                Toast.makeText(context, "输入的城市为空", Toast.LENGTH_SHORT).show();
-//                Log.i("LWT","HELLO---1");
-            }else{
-                try {
-//                    Log.i("LWT","HELLO---2");
-                    OkHttpClient client = new OkHttpClient();
-                    Request request = new Request.Builder()
-                            .url(url)
-                            .build();
-                    Response response = client.newCall(request).execute();
-                    result = response.body().toString();
-//                    Log.i("LWT","HELLO---3");
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
+        //Call入队列，异步实现
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                Log.i(TAG,"错误信息"+e.toString());
             }
-            return result;
-        }
 
-        //这里返回数据得到result
-        @Override
-        protected void onPostExecute(String result) {
-            super.onPostExecute(result);
-            Log.i("LWT",result);
-        }
+            @Override
+            public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+                Log.i(TAG,response.body().string());
+            }
+        });
     }
+
+
+
 }
